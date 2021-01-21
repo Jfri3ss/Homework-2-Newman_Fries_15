12deb7d14d8e3de598d85acbca81850bb1a5ae8d
Jfri3ss <friesjeremiah@iCloud.com>
2021-01-21 15:26:35 -0500

    Updated the formatting of the description text

--- src/concat_primes.cpp
+++ src/concat_primes.cpp
@@ -1,18 +1,20 @@
 /**
- @mainpage COP 3330 Project 2
- @Jermeiah Fries and William Newman
+@mainpage COP 3330 Project 2
+@Jermeiah Fries and William Newman
 
- As a quick disclaimer, I made most of the commits to the Github becassue William was having issues making commits on his end, we did this to save time and still shared the work equally.
- He contribuited much of the code.
+As a quick disclaimer, I made most of the commits to Github because William was having issues making commits on his end.
+we did this to save time and still shared the work equally, Williamcontribuited much of the code.
 
- We each took turns solving the coding puzzle and fixing compiling errors. We cleaned up our code, made it more human-readable and organized.
- Our program calculated every prime number up to 1000 then converts that integer into a string.
- The program then concatenates five numbers together based on the value of the start integer.
- Then the values are printed to the console and the program terminates. We accomplished this by using a for loop,
- and if statement to concatenate the values if they are primes, then a while loop to chunk the string into groups of five prime numbers.
- The engine of our program is the algorithm that we created, the algorithm iteratively loops through a number set, and uses the division comparison operator,
- if the value is equal to 0 (Primes are divisible by 1 or the number itself) then the number is added to the prime number set.
- Then at the end, we added our unit tests for our selected values.
+We each took turns solving the coding puzzle and fixing compiling errors. We cleaned up our code, made it more human-readable and organized.
+Our program calculated every prime number up to 1000 then converts that integer into a string.
+The program then concatenates five numbers together based on the value of the start integer.
+
+Then the values are printed to the console and the program terminates. We accomplished this by using a for loop,
+and if statement to concatenate the values if they are primes, then a while loop to chunk the string into groups of five prime numbers.
+
+The engine of our program is the algorithm that we created, the algorithm iteratively loops through a number set, and uses the division comparison operator,
+if the value is equal to 0 (Primes are divisible by 1 or the number itself) then the number is added to the prime number set.
+Then at the end, we added our unit tests for our selected values.
 */
 
 #include<iostream>
